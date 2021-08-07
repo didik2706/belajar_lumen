a14ea02efa8cf7f94bb95c208324783f06618ee4
didik2706 <didikhidayat27@gmail.com>
2021-08-07 14:00:06 +0700

    first commit

---
+++ coba-lumen/database/migrations/2021_08_03_232456_create_table_mahasiswa.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableMahasiswa extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_mahasiswa', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->integer('nim');
+            $table->enum('prodi', ['Informatika', 'Sistem Informasi', 'Teknik Elektro', 'Rekayasa Perangkat Lunak', 'Teknologi Informasi']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_mahasiswa');
+    }
+}
